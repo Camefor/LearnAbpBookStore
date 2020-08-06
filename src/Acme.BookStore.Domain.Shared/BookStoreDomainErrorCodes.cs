fb4fe5fd54af55429c12be8d319d127a5ccc7a3d
Camefor <2528258531@qq.com>
2020-08-06 16:26:51 +0800

    添加项目文件。

---
+++ src/Acme.BookStore.Domain.Shared/BookStoreDomainErrorCodes.cs
@@ -0,0 +1,20 @@
+﻿namespace Acme.BookStore
+{
+    public static class BookStoreDomainErrorCodes
+    {
+        /* You can add your business exception error codes here, as constants */
+    }
+
+
+    public enum BookType {
+        Undefined,
+        Adventure,
+        Biography,
+        Dystopia,
+        Fantastic,
+        Horror,
+        Science,
+        ScienceFiction,
+        Poetry
+    }
+}
