4bf0393e56bd74c974f9a7086a3831c7eb5622f4
Camefor <2528258531@qq.com>
2020-08-06 18:02:09 +0800

    第三章 集成测试

---
+++ test/Acme.BookStore.Application.Tests/BookAppService_Tests.cs
@@ -0,0 +1,70 @@
+﻿using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Xunit;
+
+namespace Acme.BookStore {
+    public class BookAppService_Tests : BookStoreApplicationTestBase {
+        private readonly IBookAppService _bookAppService;
+        public BookAppService_Tests() {
+            _bookAppService = GetRequiredService<IBookAppService>();
+        }
+
+        [Fact]
+        public async Task Should_List_Of_Books() {
+            //Act 动作
+            var result = await _bookAppService.GetListAsync(
+                    new PagedAndSortedResultRequestDto()
+                );
+            //Assert 断言
+            result.TotalCount.ShouldBeGreaterThan(0);
+            result.Items.ShouldContain(b => b.Name == "Test book 1");
+        }
+
+        /// <summary>
+        /// 测试创建一个合法book实体的场景:
+        /// </summary>
+        /// <returns></returns>
+        [Fact]
+        public async Task Should_Create_A_Valid_Book() {
+            //Act
+            var result = await _bookAppService.CreateAsync(
+                    new CreateUpdateBookDto {
+                        Name = "New test book 43",
+                        Price = 12,
+                        PublishDate = DateTime.Now,
+                        Type = BookType.ScienceFiction
+                    }
+                );
+
+            //Assert 断言
+            result.Id.ShouldNotBe(Guid.Empty);
+            result.Name.ShouldBe("New test book 43");
+        }
+
+        /// <summary>
+        /// 测试创建一个非法book实体失败的场景:
+        /// </summary>
+        /// <returns></returns>
+        [Fact]
+        public async Task Should_Not_Create_A_Book_Without_Name() {
+            var exception = await Assert.ThrowsAsync<Volo.Abp.Validation.AbpValidationException>(async () =>
+            {
+                await _bookAppService.CreateAsync(
+                    new CreateUpdateBookDto() {
+                        Name = "",
+                        Price = 10,
+                        PublishDate = DateTime.Now,
+                        Type = BookType.ScienceFiction
+                    }
+                );
+            });
+            exception.ValidationErrors.ShouldContain(err => err.MemberNames.Any(mem => mem == "Name"));
+
+        }
+    }
+}
--- test/Acme.BookStore.TestBase/BookStoreTestDataSeedContributor.cs
+++ test/Acme.BookStore.TestBase/BookStoreTestDataSeedContributor.cs
@@ -1,16 +1,47 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Volo.Abp.Data;
 using Volo.Abp.DependencyInjection;
+using Volo.Abp.Domain.Repositories;
+using Volo.Abp.Guids;
 
-namespace Acme.BookStore
-{
-    public class BookStoreTestDataSeedContributor : IDataSeedContributor, ITransientDependency
-    {
-        public Task SeedAsync(DataSeedContext context)
-        {
+namespace Acme.BookStore {
+    public class BookStoreTestDataSeedContributor
+        : IDataSeedContributor, ITransientDependency {
+        private readonly IRepository<Book, Guid> _bookRepository;
+        private readonly IGuidGenerator _guidGenerator;
+
+        public BookStoreTestDataSeedContributor(
+            IRepository<Book, Guid> bookRepository,
+            IGuidGenerator guidGenerator) {
+
+            _bookRepository = bookRepository;
+            _guidGenerator = guidGenerator;
+        }
+
+
+
+        public async Task SeedAsync(DataSeedContext context) {
             /* Seed additional test data... */
+            await _bookRepository.InsertAsync(
+                    new Book(
+                        id:_guidGenerator.Create(),
+                        name:"Test book 1",
+                        type: BookType.Fantastic,
+                        publishDate:new DateTime(2016,02,03),
+                        price:21
+                    )
+                );
 
-            return Task.CompletedTask;
+            await _bookRepository.InsertAsync(
+                   new Book(
+                       id: _guidGenerator.Create(),
+                       name: "Test book 2",
+                       type: BookType.Science,
+                       publishDate: new DateTime(2020, 08, 06),
+                       price: 65
+                   )
+               );
         }
     }
 }
